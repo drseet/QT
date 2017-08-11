feb37a0733e400493d6ce33a4a18a4c6a31c138a
root <zsb@pdx.edu>
2017-08-11 15:15:09 -0700

    added class to handle the configuration of a quiet time session

--- app/src/main/java/com/example/root/quiettime/configTime.java
+++ app/src/main/java/com/example/root/quiettime/configTime.java
@@ -2,6 +2,8 @@ package com.example.root.quiettime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class configTime extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class configTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_time);
+    }
+
+    public void onStartTap(View v) {
+        Toast.makeText(getApplicationContext(), "Start", Toast.LENGTH_LONG).show();
     }
 }
